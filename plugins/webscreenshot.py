0d4912c98f0c7ddb00d7990a267381ddf73dfdbc
ggithubb1 <95411829+ggithubb1@users.noreply.github.com>
2022-03-06 16:59:23 +0530

    Create webscreenshot.py

---
+++ plugins/webscreenshot.py
@@ -0,0 +1,25 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from wbb import app
+from plugins.http import get
+
+@Client.on_message(filters.command("ssweb"))
+async def take_ss(_, message: Message):
+    try:
+        if len(message.command) != 2:
+            return await message.reply_text("Give A Url To Fetch Screenshot.")
+        url = message.text.split(None, 1)[1]
+        m = await message.reply_text("**Taking Screenshot**")
+        await m.edit("**Uploading**")
+        ss = await get(f"https://screenshotapi1.herokuapp.com/?print={url}")['url']
+        try:
+            await message.reply_photo(
+                photo=ss,
+                quote=False,
+            )
+        except TypeError:
+            return await m.edit("No Such Website.")
+        await m.delete()
+    except Exception as e:
+        await message.reply_text(str(e))
