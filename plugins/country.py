1d46c3abda3eabb229b65bbd6441569ab25e1b36
ggithubb1 <95411829+ggithubb1@users.noreply.github.com>
2022-03-06 16:06:20 +0530

    Update country.py

--- plugins/country.py
+++ plugins/country.py
@@ -5,10 +5,11 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command('country'))
 async def countryinfo(bot, update):
-
-    country = CountryInfo(update.text)
-    
-    info = f"""**Country Information**
+ try:
+           if message.reply_to_message:
+               country = CountryInfo(message.reply_to_message.text)
+                    
+               info = f"""**Country Information**
 Name : `{country.name()}`
 Native Name : `{country.native_name()}`
 Capital : `{country.capital()}`
@@ -20,26 +21,25 @@ Calling Codes : `{country.calling_codes()}`
 Currencies : `{country.currencies()}`
 Residence : `{country.demonym()}`
 Timezone : `{country.timezones()}`"""
-    
-    reply_markup=InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton('Wikipedia', url=country.wiki()),
-                InlineKeyboardButton('Google', url=country.google())
-            ],
-            [
-                InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
-                InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
-            ]
-        ]
-    )
+                    
+               reply_markup=InlineKeyboardMarkup(
+                   [
+                       [
+                           InlineKeyboardButton('Wikipedia', url=country.wiki()),
+                           InlineKeyboardButton('Google', url=country.google())
+                       ],
+                       [
+                           InlineKeyboardButton('Channel', url='https://t.me/free_music123'),
+                           InlineKeyboardButton('Group', url='https://t.me/song_requestgroup')
+                       ]
+                   ]
+               )
     
     try:
-           if message.reply_to_message:      
-               await update.reply_text(
-                text=info,
-                reply_markup=reply_markup,
-                disable_web_page_preview=True
-            )
-        except Exception as error:
-            print(error)
+        await update.reply_text(
+            text=info,
+            reply_markup=reply_markup,
+            disable_web_page_preview=True
+        )
+    except Exception as error:
+        print(error)
